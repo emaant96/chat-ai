f53aa9aa1bdda5e65c17666d048bb280a1110d99
Emanuele <emanuele.antimiani@gmail.com>
2023-11-08 09:55:08 +0100

    added multiAsk and minors refactoring

--- backend/src/services/ai.functions.service.ts
+++ backend/src/services/ai.functions.service.ts
@@ -1,5 +1,5 @@
-import {AiFunction, businessInformation} from "../types";
-import {openapi} from "../shared/apis/openapi.api";
+import {AiFunctionEnhanced, businessInformation} from "../types";
+import {utils} from "./ai.service";
 
 
 const getInfoCompanyParameters = {
@@ -22,26 +22,13 @@ const getInfoCompanyParameters = {
 
 } as const
 
-export const functions: ({ data: AiFunction, func: ((...args: keyof AiFunction['parameters']["properties"]) => any) })[] = [
+export const functions: AiFunctionEnhanced[] = [
   {
     data: {
       name: "getInfoCompany",
       description: "get the info about a company",
       parameters: getInfoCompanyParameters
     },
-    func: getInfoCompany
+    func: utils.getCompanyInfo
   }
 ]
-
-
-export async function getInfoCompany(company_name: string, business_information: typeof businessInformation[number][]) {
-  const companies = await openapi.getCompaniesByQuery(company_name)
-  if (companies.length === 0) {
-    return `Non ho trovato nessuna azienda, riprova con un nome diverso`
-  } else if (companies.length > 1) {
-    return `ho trovato queste aziende: ${companies.map(company => company.denominazione).join(', ')}, riprova con un nome più specifico`
-  } else if (companies.length === 1) {
-    const company = await openapi.getCompanyById(companies[0].id)
-
-  }
-}
